9aef7447490351243bb1a9bdcac9c2a321e7f5b3
almeidadesv <alikaaalmeid@gmail.com>
2026-05-29 20:57:09 -0300

    html e css concluidos

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ function conversorDeMoedas (){
 
 function calcularImc () {
 
+let altura = ""
+let peso = ""
+let imc = peso / altura**2 
+console.log (`O valor do imc é de: ${imc.toFixed(2)}`)
+
 }  
 
 
