4fc2c94a5f76e3ffb5216ad6b762395d5a73ca99
almeidadesv <alikaaalmeid@gmail.com>
2026-06-01 22:09:19 -0300

    trocando os paineis

--- script.js
+++ script.js
@@ -1,35 +1,88 @@
-function conversorDeMoedas (){
-
+function esconderPaineis() {
+    document.querySelectorAll("#ferramentas > div").forEach(div => {
+        div.classList.add("hidden");
+    });
 }
 
+document.getElementById("puxarmoeda").onclick = () => {
+    esconderPaineis();
+    document.querySelector(".painelmoeda").classList.remove("hidden");
+};
 
+document.getElementById("puxarimc").onclick = () => {
+    esconderPaineis();
+    document.querySelector(".painelimc").classList.remove("hidden");
+};
 
-function calcularImc () {
+document.getElementById("puxartemperatura").onclick = () => {
+    esconderPaineis();
+    document.querySelector(".paineltemperatura").classList.remove("hidden");
+};
 
-let altura = ""
-let peso = ""
-let imc = peso / altura**2 
-(`O valor do imc é de: ${imc.toFixed(2)}`)
+document.getElementById("puxarvelocidade").onclick = () => {
+    esconderPaineis();
+    document.querySelector(".painelvelocidade").classList.remove("hidden");
+};
 
-}  
+document.getElementById("puxarmassa").onclick = () => {
+    esconderPaineis();
+    document.querySelector(".painelmassa").classList.remove("hidden");
+};
 
+document.getElementById("regratres").onclick = () => {
+    esconderPaineis();
+    document.querySelector(".painelregra").classList.remove("hidden");
+};
+
+esconderPaineis()
+
+//
+
+function conversorDeMoedas (){
+}
+
+//
+
+document.querySelector(".botao-imc").onclick = calcularImc;
+
+function calcularImc() {
+    let peso = +document.querySelector(".pesoDigitado").value.trim();
+    let altura = +document.querySelector(".alturaDigitada").value.trim();
+
+    let imc = peso / altura ** 2;
+    let classificacao = "";
+
+    if      (imc < 18.5) classificacao = "Abaixo do peso";
+    else if (imc < 25)   classificacao = "Peso normal";
+    else if (imc < 30)   classificacao = "Sobrepeso";
+    else                 classificacao = "Obesidade";
+
+    document.getElementById("resultadoIMC").innerHTML =
+        "IMC: " + imc.toFixed(2) + " = " + classificacao;
+}
+
+calcularImc()
+
+//
 
 
 function conversorDeTemperatura () {
 
 }
 
-
+//
 
 function conversorDeVelocidade () {
 
 }
 
-
+//
 
 function conversorDeMassa () {
 
 }
+
+//
 
 function regraDeTres () {
     
---
+++ scriptdois.js
@@ -0,0 +1,4 @@
+function esconderTudo () {
+    document.querySelectorAll("div");
+    div.classList.add("hidden")
+}
